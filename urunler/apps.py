004a86c62915b93143f9add523a32b0ae75ef6f1
Mehmet Eski <157639357+mehmeteski-42@users.noreply.github.com>
2025-04-20 23:11:36 +0300

    Delete urunler directory

--- urunler/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UrunlerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'urunler'
--- urunler/utils.py
+++
@@ -1,9 +0,0 @@
-# urunler/utils.py
-
-def convert_to_base_unit(product, quantity, unit_input):
-    if product.unit_type == "adet-koli":
-        if unit_input == "koli":
-            return quantity * product.unit_per_package
-        elif unit_input == "adet":
-            return quantity
-    return quantity
